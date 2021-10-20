096ece9e06f66c206aedc495d1686b0e2a054abc
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-20 13:09:47 +0200

    Raten Probieren/ While Loops & break

---
+++ raten.py
@@ -0,0 +1,26 @@
+#rate
+
+#variable
+zahl = 14
+raten = -9999
+
+#main
+print("Eine Zahl zwwischen 0 und 20 ist gesucht.")
+
+while raten != zahl:
+    raten = int(input("bitte raten sie: "))
+    print("falsch")
+
+else:
+    print("richtig!")
+
+# Anzahl Abfragen
+
+if x == 1: 
+        print("access denied")
+elif x == 3:
+        print("you have 2 more trials")
+elif x == 2:
+        print("you have 1 more trials")
+else:
+        print("password not accepted")
---
+++ raten_break.py
@@ -0,0 +1,15 @@
+#rate
+
+#main
+print("Eine Zahl zwwischen 0 und 20 ist gesucht.")
+for x in range (3, 0, -1): 
+    #variable
+    zahl = 14
+    raten = -9999
+
+while raten != zahl:
+    raten = int(input("bitte raten sie: "))
+    print("falsch")
+
+else:
+    print("richtig!")
