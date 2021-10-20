fe8f116dfc6bbcad36be44705c919e6e9887b906
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-20 13:10:11 +0200

    while schleifen Break beenden

---
+++ VL2.py
@@ -0,0 +1,15 @@
+# Kontrollsturkturen
+# Variablen definieren 
+
+x = 3
+
+if x > 1:
+    print("x hat den Wert: ", x)
+
+elif x==1:
+    print("x hat den Wert 1)")
+else: 
+    print("x ist ", x)
+
+print("done.")
+
