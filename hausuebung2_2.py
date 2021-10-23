880267ae7331c2d86be2f1ddf1943e39f3ac619b
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-23 14:37:24 +0200

    Testen GROSS/klein (fehlersuche)

--- hausuebung2_2.py
+++ hausuebung2_2.py
@@ -1,11 +1,30 @@
 # Input soll umgewandelt werden FOR Loop hierfür verwenden
 
 #1 : Hier wird die Eingabe pro Buchstabe  nach Upper/Lower getestet und dann jeweills gegeneteilig zurückgegeben
+# eingabe = input("Enter something:")
+# for x in eingabe:
+#     if x >= 'A' and x <= 'Z':
+#             print(x.lower())
+#     elif x >= 'a' and x <= 'z':
+#             print(x.upper())
+
+# print("done")
+
+# eingabe = input("Enter something:")
+# for x in eingabe:
+#     if x >= 'A' and x <= 'Z':
+#             print(x.islower())
+#     else:
+#             print("TRUE")
+
+# print("done")
+
 eingabe = input("Enter something:")
 for x in eingabe:
     if x >= 'A' and x <= 'Z':
-            print(eingabe.lower())
-    elif x >= 'a' and x <= 'z':
-            print(eingabe.upper())
+            print("false")
+    else:
+            print("true")
 
 print("done")
+
