735d2bd8e6bf35de3d0c2cd61982af18f0092b1b
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-23 14:16:24 +0200

    Eingabe je Buchstabe nach Upper/Lower testen

--- hausuebung2_2.py
+++ hausuebung2_2.py
@@ -1,7 +1,11 @@
 # Input soll umgewandelt werden FOR Loop hierfür verwenden
 
+#1 : Hier wird die Eingabe pro Buchstabe  nach Upper/Lower getestet und dann jeweills gegeneteilig zurückgegeben
 eingabe = input("Enter something:")
 for x in eingabe:
-    x = eingabe.upper()
-    print(x)
+    if x >= 'A' and x <= 'Z':
+            print(eingabe.lower())
+    elif x >= 'a' and x <= 'z':
+            print(eingabe.upper())
 
+print("done")
