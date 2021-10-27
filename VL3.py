b351d18bb77a196c1b0df197c2421724e0c84de6
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-27 12:37:45 +0200

    Funktion einer Variablen zuordnen, um mit dieser weiterrechnen zu können

--- VL3.py
+++ VL3.py
@@ -8,10 +8,15 @@ def fakultaet(zahl): # hier die Variable "Zahl" durch in die Klammern schreiben 
     
     for i in range(2, zahl+1): # i muss man in dem Fall nicht definieren, da es jeweils i-te Stelle darstellt
         ergebnis = ergebnis * i
-    print("Ergebins: ", ergebnis)
+    # print("Ergebins: ", ergebnis)
+    # bis hierher bekomme ich nur ein Ergebnis. WEnn ich mit dem Ergebnis weiterrechnen will brauche ich eine weitere Funktion:
+    return ergebnis
+
 # MAIN
 while True:
     zahl = int(input("Eingabe: "))
-    fakultaet(zahl) # DAS ist der AUFRUF bzw. DURCHFÜHRUNG der Funktion!!! 
+    # fakultaet(zahl) # DAS ist der AUFRUF bzw. DURCHFÜHRUNG der Funktion!!! 
+    var = fakultaet(zahl) # gibt dem Ergebnis der Funktion eine Variable. Mit dieser kann dann im "Hauptprogramm" weitergerechnet werden
+    print(var)
 
 print("done")
