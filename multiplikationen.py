1b2546a9b1d68054e660db86d51406962b6688be
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-27 12:45:01 +0200

    erste multofunktionen

---
+++ multiplikationen.py
@@ -0,0 +1,17 @@
+# Multiplikationen
+
+# MODULES --> Struktur angewöhnen
+
+# FUNCTIONS
+def multi(a,b,c,d):
+    var = a*b*c*d
+    return var
+
+#MAIN
+
+# erg1= multi(4,2) #hier Variablen definieren! Hier falsch, weil c & d nicht definiert
+# erg2 =multi(4,2,6)
+erg3 = multi(4,2,6,6) # Ab hier läuft das Programm, weil alle variablen definiert wurden
+
+print(erg3)
+print("done")
