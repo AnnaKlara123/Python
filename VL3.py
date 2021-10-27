10ce690d168eb30bc4ea2ae12fc94d9b6c4b3554
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-27 12:29:22 +0200

    Funktion schreiben, zahl definieren

--- VL3.py
+++ VL3.py
@@ -3,7 +3,12 @@
 # MODULES --> Struktur angewöhnen
 
 # FUNCTIONS
-
+def fakultaet(zahl): # hier die Variable "Zahl" durch in die Klammern schreiben defiieren
+    ergebnis = 1
+    
+    for i in range(2, zahl+1): # i muss man in dem Fall nicht definieren, da es jeweils i-te Stelle darstellt
+        ergebnis = ergebnis * i
+    print("Ergebins: ", ergebnis)
 # MAIN
 
 print("done")
