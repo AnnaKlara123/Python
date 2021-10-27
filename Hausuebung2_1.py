3b4a7be38ce332624ff3e4760ace895125c45fd5
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-27 12:13:22 +0200

    Beginn VL3

--- Hausuebung2_1.py
+++ Hausuebung2_1.py
@@ -4,7 +4,9 @@ eingabe = input("Enter something:")
       
 for i in eingabe:
     try:
-        x = int(eingabe)
+        x = int(eingabe) # testet, ob eingabe ein Intiger ist, wenn ja, dann ist es KEIN String!
         print(eingabe, "is not a string")
-    except:
+    except ValueError:                # Wenn es kein Intiger ist(ValueError), ist es ein String (Exception)
         print(eingabe, "is a string") 
+
+print("done")
--- hausuebung2_2.py
+++ hausuebung2_2.py
@@ -1,30 +1,13 @@
 # Input soll umgewandelt werden FOR Loop hierfür verwenden
 
 #1 : Hier wird die Eingabe pro Buchstabe  nach Upper/Lower getestet und dann jeweills gegeneteilig zurückgegeben
-# eingabe = input("Enter something:")
-# for x in eingabe:
-#     if x >= 'A' and x <= 'Z':
-#             print(x.lower())
-#     elif x >= 'a' and x <= 'z':
-#             print(x.upper())
-
-# print("done")
-
-# eingabe = input("Enter something:")
-# for x in eingabe:
-#     if x >= 'A' and x <= 'Z':
-#             print(x.islower())
-#     else:
-#             print("TRUE")
-
-# print("done")
 
 eingabe = input("Enter something:")
 for x in eingabe: # zählt jeden Buchstaben einzeln
     if x.isupper(): # checkt, ob Buchstabe GROSS oder klein ist
             print(x.lower()) # gibt Buchstabe jeweils gegenteilig zurück
     elif x.islower():
-            print(x.upper())
+            print(x.upper())    
 
 print("done")
 
