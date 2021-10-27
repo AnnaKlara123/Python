9590eba2e468bc123ea021d8c52d087de2e5af61
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-27 12:49:25 +0200

    Multiplikationen mit jeweils individuel definierten Variabelwerten

--- multiplikationen.py
+++ multiplikationen.py
@@ -11,7 +11,14 @@ def multi(a,b,c,d):
 
 # erg1= multi(4,2) #hier Variablen definieren! Hier falsch, weil c & d nicht definiert
 # erg2 =multi(4,2,6)
-erg3 = multi(4,2,6,6) # Ab hier läuft das Programm, weil alle variablen definiert wurden
+# erg3 = multi(4,2,6,6) # Ab hier läuft das Programm, weil alle variablen definiert wurden
+wert1 = 4
+wert2 = 2
+wert3 = 6
+wert4 = 6
 
-print(erg3)
+#funktionsaufruf
+erg1 = (wert1, wert2, wert3, wert4)
+
+print(erg1)
 print("done")
