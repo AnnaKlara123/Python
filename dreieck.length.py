cd40350491617de9c61e8c6f28e38ccea4511f90
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-28 13:12:19 +0200

    Aufgabe Längenberechnung abgeschlossen

--- dreieck.length.py
+++ dreieck.length.py
@@ -5,14 +5,14 @@
 # Funktionen
 import math
 
-def dist(x1, x2, y1, y2): # Funktion erwartet 4 Argumente
+def dist(x1, x2, y1, y2): # Funktion deniniert: sie erwartet 4 Argumente und inkludiert 3 Rechnungen 
     dx = x2 - x1
     dy = y2 -y1
     d = math.sqrt(dx**2 + dy**2)
-    return d
+    return d # d ist die Länge!
 
 # Main
-i = list(map(int, input("Numbers: ").split())) 
+i = list(map(int, input("Numbers: ").split())) # Eingabe als Liste einlesen 
 print(i) 
 
 x1 = i[0]
@@ -20,21 +20,16 @@ x2 = i[1]
 y1 = i[2]
 y2 = i[3]
 
-print (x1)
-print (x2)
-print (y1)
-print (y2)
+# print (x1)
+# print (x2)
+# print (y1)
+# print (y2)
 
-
-# x1 = 1
-# x2 = 2
-# y1 = 3
-# y2 = 4
-
+# Call function
 var2 = dist(x1,x2, y1, y2)
 print(var2)
 
-# Call function
+
  # Direkte Berechnung mit vorgegebennen Zahlen 
     # var1 = dist(1,2,3,4) # hier werden Argumente direkt der Funktion übergeben / Hier wird Funktuon aufgerufen
     # print(var1)
