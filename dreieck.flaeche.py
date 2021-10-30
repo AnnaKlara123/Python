b49b83a0939001ea17bc342f1d4f5e6c0feedc5f
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-30 08:55:21 +0200

    Fläche eines Dreiecks in drei Iterationen berechnen

--- dreieck.flaeche.py
+++ dreieck.flaeche.py
@@ -12,8 +12,22 @@ def area(h, k): # Funktion deniniert: sie erwartet 2 Argumente und inkludiert 1 
 # Main --> Parameter definieren durch input 
 h = int(input("Geben  Sie die Höhe ein des Dreiecks:"))
 k = int(input("Geben Sie Kantenlänge  der Grundseite ein:"))
+iterat= int(input("Geben sie die Wiederholungen ein"))
+weit = int(input("Schrittweite: "))
 
-
+for i in range(0, iterat,1): # Schrittzahl eventuell später noch definieren
 # Call function
-area = area(h, k)
-print(area)
+    #schrittl = schrittl+liste[5]
+    #var2 = dist(x1+schrittl,x2+schrittl, y1+i, y2+schrittl) # hier rechne ich immer PLUS die i-te Iteration 
+    if i == 0:
+        h = h+weit
+        k = k
+        area1 = area(h, k)
+        print(i)
+        print("die Fläche beträgt: ", area1)
+    else:
+        h = h+weit
+        k = k+weit
+        area2 = area(h, k)
+        print(i)
+        print("die Fläche beträgt: ", area2)
