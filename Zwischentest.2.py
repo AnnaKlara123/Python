160588bf73c4c31ff2c9211dd44c49a188006bdc
Anna Stang <anna.stang@student.uibk.ac.at>
2021-11-01 00:54:17 +0100

    Vol.berechnung über i iterationen

--- Zwischentest.2.py
+++ Zwischentest.2.py
@@ -24,27 +24,24 @@ for i in range(1,11,1):
         print(i)
         vol = qurd(s) # Berechnung des WürfelVolumens erste Iteration
         # print(vol)
-        print("Das Volumen des" , 1 ,"ten Würfels beträgt", vol)
+        print("Das Volumen des" , 1 ,"ten Würfels beträgt", vol, "m^3")
         volk = kugel(r) #Berechnung des Kugelvolumens erste Iteration
         # print(volk)
-        print("Das Volumen der" , 1 ,"ten Kugel beträgt", volk)
+        print("Das Volumen der" , 1 ,"ten Kugel beträgt", volk, "m^3")
         vw = vol
         vk = volk
         absolutdiff = dif(vw, vk) # Berechnung der Differenz der beiden Volumen 
         print("Die Differenz beträgt: ", absolutdiff)
         
-    else:
-        vol2=qurd(s+i)
-        print(i)
-        
-        volk2=kugel(r+i)
-        print(i)
-
-        absolutdiff = dif(vw, vk)
-    print("Das Volumen des" , i ,"ten Würfels beträgt", vol2)
-    print("Das Volumen des" , i ,"ten Würfels beträgt", volk2)
-    print("Die Differenz beträgt: ", absolutdiff)
-
-
+    elif i<11 and i>1:
+            print(i)
+            vol2=qurd(s+i)        
+            volk2=kugel(r+i)
+            vw = vol2
+            vk = volk2
+            absolutdiff = dif(vw, vk)
+            print("Das Volumen des" , i ,"ten Würfels beträgt", vol2, "m^3")
+            print("Das Volumen des" , i ,"ten Würfels beträgt", volk2, "m^3")
+            print("Die Differenz beträgt: ", absolutdiff)
 print("done")
 
