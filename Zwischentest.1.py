4500ffe8a28149c5e35d18c8adb765dfc2f96639
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-31 21:33:04 +0100

    Dritte Variante mit Liste und Großbuchstaben

--- Zwischentest.1.py
+++ Zwischentest.1.py
@@ -4,6 +4,16 @@ satz = input('Gib einen Sazt ein: ')
 wordcount = len(satz.split())
 print("Der Satz hat", wordcount, "Wörter")
 
+upper = 0 # eine Count Variable anlegen, die sich erhöt, sobald ein Großbuchstabe erkannt wird
+for i in satz:
+    #zählt Großbuchstaben
+    if(i.isupper()):
+        upper = upper + 1
+		
+
+# Ausgabe der gezählten Buchstaben 
+print('Der Satz hat',upper, 'Großbuchstaben')
+
 #Alternativ:
 # Satz eingeben 
 satz = input("gib einen Satz ein: ")
@@ -12,14 +22,16 @@ upper_count = sum(1 for i in satz if i.isupper())
 # print 
 print("der Sazt hat", upper_count, "Großbuchstaben")
 
+#Alternative 2
 
-upper = 0 # eine Count Variable anlegen, die sich erhöt, sobald ein Großbuchstabe erkannt wird
-for i in satz:
-    #zählt Großbuchstaben
-    if(i.isupper()):
-        upper = upper + 1
+wordcount = len(satz.split())
+list = satz.split()
+print(list)
+count = 0
+for i in list:
+    if i[0].isupper():
+        count = count + 1
+		
+print(count , "Großbuchstaben")
 
-# Ausgabe der gezählten Buchstaben 
-print('Der Satz hat',upper, 'Großbuchstaben')
-
-
+print("done")
