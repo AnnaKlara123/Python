bc05c90f69d0666853d62f8f8bf6d167fc6883d5
Anna Stang <anna.stang@student.uibk.ac.at>
2021-10-31 21:11:57 +0100

    Alternative Großbuchstabensuche mit SUM

--- Zwischentest.1.py
+++ Zwischentest.1.py
@@ -4,6 +4,15 @@ satz = input('Gib einen Sazt ein: ')
 wordcount = len(satz.split())
 print("Der Satz hat", wordcount, "Wörter")
 
+#Alternativ:
+# Satz eingeben 
+satz = input("gib einen Satz ein: ")
+# Großbuchstaben zählen
+upper_count = sum(1 for i in satz if i.isupper())
+# print 
+print("der Sazt hat", upper_count, "Großbuchstaben")
+
+
 upper = 0 # eine Count Variable anlegen, die sich erhöt, sobald ein Großbuchstabe erkannt wird
 for i in satz:
     #zählt Großbuchstaben
@@ -12,3 +21,5 @@ for i in satz:
 
 # Ausgabe der gezählten Buchstaben 
 print('Der Satz hat',upper, 'Großbuchstaben')
+
+
