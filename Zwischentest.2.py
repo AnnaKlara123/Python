f442d0d90b9e14f1651d809859a0d7adf11c634a
Anna Stang <anna.stang@student.uibk.ac.at>
2021-11-01 01:12:17 +0100

    Zwischenspeicherung

--- Zwischentest.2.py
+++ Zwischentest.2.py
@@ -3,23 +3,23 @@ import math
 
 # Funktion def. 
 def qurd(s):
-    vw = s**3
+    vw = s**3 # Volumen Würfel Formel
     return vw
 
 def kugel(r):
     pi = math.pi
-    vk = (4/3)*pi*r**3
+    vk = (4/3)*pi*r**3 # Volumen Kugel Formel
     return vk
 
 def dif(vw,vk):
-    differ = abs(vw-vk)
+    differ = abs(vw-vk) # Differenz der Volumen berechnen
     return differ
 
-#Main
+#Main - Usereingabe
 s=r = int(input("Geben Sie die Seitenlänge, bzw. den Radius an: "))
 
-for i in range(1,11):
-    if i == 1:
+for i in range(1,11): #start=1, Ende bei 10, 1= Steps
+    if i == 1: # Berechnung der 1.Runde, ohne "count"
         print(i,"te Iteration")
         vw = qurd(s) # Berechnung des WürfelVolumens durch Aufrufen der Funktion quad als erste Iteration und Speicherung des Werts als Variable
         print("Das Volumen des" , 1 ,"ten Würfels beträgt", vw, "m^3")
@@ -31,7 +31,7 @@ for i in range(1,11):
         
     elif i<11 and i>1: # Berechnung des Volumens für die anderen 9 Iterationen
             print(i,"te Iteration")
-            vw=qurd(s+i)        # +i um jeweils Plus 1 zu rechnen 
+            vw=qurd(s+i)        # +i um jeweils plus 1 zu rechnen 
             vk=kugel(r+i)
             # vw = vol2
             # vk = volk2
